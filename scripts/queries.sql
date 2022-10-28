799500b21ea65f6cc3787aa213ba6beece0aef11
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-10-28 11:56:20 +0200

    add first join sample

--- scripts/queries.sql
+++ scripts/queries.sql
@@ -1,9 +1,8 @@
---SELECT * FROM BOOKS;
--- Kreuprodukt: Alle Zeilen aus AUTHORS kombiniert mit allen Zeilen von ADDRESSES
--- SELECT * FROM AUTHORS, ADDRESSES;
--- Kreuzprodukt mit Kriterium
---SELECT * FROM AUTHORS, ADDRESSES WHERE LASTNAME='Pausenkurt';
---SELECT * FROM AUTHORS, ADDRESSES WHERE ID=1; --GEHT NICHT, ID ambiguos
---SELECT * FROM AUTHORS, ADDRESSES WHERE AUTHORS.ID = 1;
---SELECT AUTHORS.ID AS AUTHOR_ID, ADDRESSES.ID AS ADDRESS_ID FROM AUTHORS, ADDRESSES WHERE AUTHORS.ID = 1;
---SELECT AU.ID AS AUTHOR_ID, AD.ID AS ADDRESS_ID FROM AUTHORS AS AU, ADDRESSES AS AD WHERE AU.ID = 1;
+--Ich benötige alle Bücher des Verlegers Publisher3
+--Versuch: Kreuzprodukt -> Funktioiert so nicht
+--SELECT B.TITLE, P.NAME FROM PUBLISHERS AS P, BOOKS AS B WHERE NAME='Publisher3';
+--Versuch: 2 Selektionen, erst die ID des Publishers, dann eine Selektion auf PUBLISHER_ID -> Funktioniert
+--SELECT P.ID FROM PUBLISHERS AS P WHERE P.NAME = 'Publisher3';
+--SELECT B.TITLE FROM BOOKS S B WHERE B.PUBLISHER_ID = 3;
+--JOIN ... ON ... -> Bedingung, die aus dem Kreuzprodukt nur die passenden Kombinationen filtert
+SELECT B.TITLE FROM BOOKS AS B JOIN PUBLISHERS AS P ON P.ID = B.PUBLISHER_ID  WHERE P.NAME = 'Publisher3'
