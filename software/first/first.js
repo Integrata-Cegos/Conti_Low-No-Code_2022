5172fe0bd5544b58c7404a441a56919d6bbe53a8
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-11-25 11:21:28 +0100

    introduce variables

--- software/first/first.js
+++ software/first/first.js
@@ -1,1 +1,9 @@
+//Kommentarzeile
+//Deklaration von Variablen
+welcomeMessage = "Hello World"
+itemCount = 20
+itemPrice = 42
+//Aufruf der log-Funktion der Konsole mit dem Parameter Zeichenkette "Hello World!!!"
 console.log("Hello World!!!")
+//Aufruf der log-Funktion der Konsole mit dem Parameter welcomeMessage
+console.log(welcomeMessage)
