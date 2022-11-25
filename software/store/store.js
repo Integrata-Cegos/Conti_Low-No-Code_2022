ff2314b25e5763559b050d5c587cb351d17bc0d7
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-11-25 15:42:14 +0100

    add repitition

--- software/store/store.js
+++ software/store/store.js
@@ -1,15 +1,18 @@
-input = prompt()
-itemCount = parseInt(input)
-if (itemCount > 0){
-    discount = 0
-    if (itemCount >= 10 && itemCount < 20){
-        discount = 0.05
-    }else if (itemCount >= 20){
-        discount = 0.1
+do {
+    input = prompt()
+    itemCount = parseInt(input)
+    if (itemCount > 0) {
+        discount = 0
+        if (itemCount >= 10 && itemCount < 20) {
+            discount = 0.05
+        } else if (itemCount >= 20) {
+            discount = 0.1
+        }
+        itemPrice = 42
+        totalPrice = itemPrice * itemCount * (1 - discount)
+        console.log("Total price is " + totalPrice)
+    } else {
+        console.log("invalid input")
     }
-    itemPrice = 42
-    totalPrice = itemPrice * itemCount * (1 - discount)
-    console.log("Total price is " + totalPrice)
-}else{
-    console.log("invalid input")
-}
+    doAgain = prompt("again?")
+} while (doAgain != "exit");
