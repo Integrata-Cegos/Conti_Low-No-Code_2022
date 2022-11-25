e1daaf5acaccbf7de43ea32e6affc80a69778e0b
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-11-25 11:44:35 +0100

    add discount

--- software/first/first.js
+++ software/first/first.js
@@ -1,9 +1,11 @@
-//Kommentarzeile
-//Deklaration von Variablen
 welcomeMessage = "Hello to Training Application"
 console.log(welcomeMessage)
-itemCount = 20 //Später: kann eine Eingabe sein
-itemPrice = 42 //Später: kann eine Eingabe sein
+itemCount = 20 
+itemPrice = 42 
 totalPrice = 20*42
-result = "TotalPrice: " + totalPrice
-console.log(result)
+totalPriceOutput = "TotalPrice: " + totalPrice
+console.log(totalPriceOutput)
+discount = 0.1
+totalPrice = totalPrice * (1 - discount)
+totalPriceOutput = "TotalPrice with 10% discount: " + totalPrice
+console.log(totalPriceOutput)
