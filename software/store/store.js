364e986b6063227a3bf747db72cd752f4de7d9fd
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-11-25 15:15:20 +0100

    add discount

--- software/store/store.js
+++ software/store/store.js
@@ -1,8 +1,14 @@
 input = prompt()
 itemCount = parseInt(input)
 if (itemCount > 0){
+    discount = 0
+    if (itemCount >= 10 && itemCount < 20){
+        discount = 0.05
+    }else if (itemCount >= 20){
+        discount = 0.1
+    }
     itemPrice = 42
-    totalPrice = itemPrice * itemCount
+    totalPrice = itemPrice * itemCount * (1 - discount)
     console.log("Total price is " + totalPrice)
 }else{
     console.log("invalid input")
