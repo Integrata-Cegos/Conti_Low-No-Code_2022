932d4f714101f9d9dfba9690242c3ac740491bcb
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-11-25 14:03:17 +0100

    implement simple validation

--- software/store/store.js
+++ software/store/store.js
@@ -1,5 +1,12 @@
-itemCount = parseInt(prompt("Number of items:"))
-itemPrice = 42 
-totalPrice = itemPrice * itemCount
-totalPriceOutput = "Total price: " + totalPrice
-console.log(totalPriceOutput)
+input = prompt("Number of items:")
+itemCount = parseInt(input)
+if (itemCount != NaN && itemCount > 0){
+    itemPrice = 42 
+    totalPrice = itemPrice * itemCount
+    totalPriceOutput = "Total price: " + totalPrice
+    console.log(totalPriceOutput)
+    
+}
+else{
+    console.log(input + " is not a positive number")
+}
