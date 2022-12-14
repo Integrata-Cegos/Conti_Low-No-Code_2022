be461e58b7a913565d661c3527e5d98e5f9b208e
Detlef Roeder <detlef.roeder@continental.com>
2022-12-14 14:43:59 +0100

    Bem raus mit no ergänzt

--- software/store/store.js
+++ software/store/store.js
@@ -1,18 +1,24 @@
+let doAgain = ""
+let discounts = {
+    no: 0,
+    low: 0.05,
+    high: 0.1
+}
 do {
-    input = prompt()
-    itemCount = parseInt(input)
+    let input = prompt()
+    let itemCount = parseInt(input)
     if (itemCount > 0) {
-        discount = 0
+        let discount = discounts.no
         if (itemCount >= 10 && itemCount < 20) {
-            discount = 0.05
+            discount = discounts.low
         } else if (itemCount >= 20) {
-            discount = 0.1
+            discount = discounts.high
         }
-        itemPrice = 42
-        totalPrice = itemPrice * itemCount * (1 - discount)
+        let itemPrice = 42
+        let totalPrice = itemPrice * itemCount * (1 - discount)
         console.log("Total price is " + totalPrice)
     } else {
         console.log("invalid input")
     }
-    doAgain = prompt("again?")
-} while (doAgain != "exit");
+    doAgain = prompt("again? raus mit no!")
+} while (doAgain != "no");
