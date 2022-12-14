8d6b98e7774f7e73f9f0c73ef0180aea730a3555
Tanja Brost <tanja.brost@continental-corporation.com>
2022-12-14 14:16:51 +0100

    introduce let

--- software/store/store.js
+++ software/store/store.js
@@ -1,15 +1,16 @@
+let doAgain = ""
 do {
-    input = prompt()
-    itemCount = parseInt(input)
+    let input = prompt()
+    let itemCount = parseInt(input)
     if (itemCount > 0) {
-        discount = 0
+        let discount = 0
         if (itemCount >= 10 && itemCount < 20) {
             discount = 0.05
         } else if (itemCount >= 20) {
             discount = 0.1
         }
-        itemPrice = 42
-        totalPrice = itemPrice * itemCount * (1 - discount)
+        let itemPrice = 42
+        let totalPrice = itemPrice * itemCount * (1 - discount)
         console.log("Total price is " + totalPrice)
     } else {
         console.log("invalid input")
