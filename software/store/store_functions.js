59304026eabfc184eae6542424d42736259e1be8
Detlef Roeder <detlef.roeder@continental.com>
2022-12-14 15:51:38 +0100

    mit functions

---
+++ software/store/store_functions.js
@@ -0,0 +1,39 @@
+function loop(){
+    let doAgain = ""
+    let exitMessage = "no"
+    do {
+        userInput()
+        doAgain = prompt("again? - enter " + exitMessage + " to exit program")
+    } while (doAgain != exitMessage);
+}
+
+function userInput(){
+    let input = prompt()
+    let itemCount = parseInt(input)
+    if (itemCount > 0) {
+        let totalPrice = calculatePrice(itemCount)
+        console.log("Total price is " + totalPrice)
+    } else {
+        console.log("invalid input")
+    }
+}
+
+function calculatePrice(numberOfItems){
+    let discounts = {
+        no: 0,
+        low: 0.05,
+        high: 0.1
+    }
+    let discount = discounts.no
+    if (numberOfItems >= 10 && numberOfItems < 20) {
+        discount = discounts.low
+    } else if (numberOfItems >= 20) {
+        discount = discounts.high
+    }
+    let itemPrice = 42
+    let totalPrice = itemPrice * numberOfItems * (1 - discount)
+    return totalPrice
+
+}
+
+loop()
