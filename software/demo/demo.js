405b8b4f54b438f3313b3616eac4e67ff10191dd
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-12-14 14:58:16 +0100

    intrdouce functions

--- software/demo/demo.js
+++ software/demo/demo.js
@@ -1,9 +1,19 @@
-input = prompt()
-itemCount = parseInt(input)
-if (itemCount > 0){
-    itemPrice = 42
-    totalPrice = itemPrice * itemCount
-    console.log("Total price is " + totalPrice)
-}else{
-    console.log("invalid input")
+let person = {
+    lastname: "Sawitzki", 
+    firstname: "Rainer"
 }
+
+console.log(person.lastname)
+let first = person.firstname
+console.log(first)
+person.height = 183
+console.log(person.height)
+
+function sayHello(name){
+    return "Hello " + name
+}
+
+console.log(sayHello("Hugo"))
+console.log(sayHello("Emil"))
+let helloMessage = sayHello(person.lastname)
+console.log(helloMessage)
