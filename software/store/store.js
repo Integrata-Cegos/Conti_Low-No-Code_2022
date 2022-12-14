531395ee7507a9bdc7242ecdc25d2f2f19164f0a
Tanja Brost <tanja.brost@continental-corporation.com>
2022-12-14 15:26:40 +0100

    Add functions

--- software/store/store.js
+++ software/store/store.js
@@ -1,25 +1,36 @@
-let doAgain = ""
-let discounts = {
-    no: 0,
-    low: 0.05,
-    high: 0.1
+function loop*(){
+    let doAgain = ""
+    do {
+        userInput()
+        doAgain = prompt("again? - no to exit")
+    } while (doAgain != "no");
 }
-do {
+function userInput(){
     let input = prompt()
     let itemCount = parseInt(input)
     if (itemCount > 0) {
-        let discount = 0
-        discount = discounts.no
-        if (itemCount >= 10 && itemCount < 20) {
-            discount = discounts.low
-        } else if (itemCount >= 20) {
-            discount = discounts.high
-        }
-        let itemPrice = 42
-        let totalPrice = itemPrice * itemCount * (1 - discount)
+        let totalPrice = calculatePrice(itemCount)
         console.log("Total price is " + totalPrice)
     } else {
         console.log("invalid input")
     }
-    doAgain = prompt("again?")
-} while (doAgain != "exit");
+
+}
+function calculatePrice(numberOfItems){
+    let discounts = {
+        no: 0,
+        low: 0.05,
+        high: 0.1
+    }
+    let discount = discounts.no
+    if (numberOfItems >= 10 && numberOfItems < 20) {
+        discount = discounts.low
+    } else if (numberOfItems >= 20) {
+        discount = discounts.high
+    }
+    let itemPrice = 42
+    let totalPrice = itemPrice * numberOfItems * (1 - discount)
+    return totalPrice
+}
+
+loop()
