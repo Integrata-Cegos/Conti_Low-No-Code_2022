fd55ccfcc345093d599001eec5c60717efb69676
Linh Nguyen <thuy.linh.nguyen@continental.com>
2022-12-15 11:07:58 +0100

    change content

--- software/tablet/tablet.js
+++ software/tablet/tablet.js
@@ -1,36 +1,16 @@
-let inputElement = document.getElementById("input")
-let resultElement = document.getElementById("result")
+let articleIdInput = document.getElementById("articleId")
+let numberOfArticlesInput = document.getElementById("numberOfArticles")
+let totalPriceOutput = document.getElementById("totalPrice")
 
-function userInput(){
-    let input = inputElement.value
-    let itemCount = parseInt(input)
-    if (itemCount > 0) {
-        let totalPrice = calculatePrice(itemCount)
-        resultElement.value = "Total price is " + totalPrice
-    } else {
-        resultElement.value = "invalid input"
-    }
-}
-
-function clearInput(){
-    inputElement.value = ""
+function calculatePrice(){
+    let articleId = articleIdInput.value
+    let numberOfArticles = parseInt(numberOfArticlesInput.value)
+    let totalPrice = 42
+    let result = createTotalPriceMessage(articleId, numberOfArticles, totalPrice)
+    totalPriceOutput.value = result
 
 }
 
-function calculatePrice(numberOfItems){
-    let discounts = {
-        no: 0,
-        low: 0.05,
-        high: 0.1
-    }
-    let discount = discounts.no
-    if (numberOfItems >= 10 && numberOfItems < 20) {
-        discount = discounts.low
-    } else if (numberOfItems >= 20) {
-        discount = discounts.high
-    }
-    let itemPrice = 5
-    let totalPrice = itemPrice * numberOfItems * (1 - discount)
-    return totalPrice
-
+function createTotalPriceMessage(articleId, numberOfArticles, totalPrice){
+    return `total price: ${numberOfArticles} articles with id ${articleId} cost: ${totalPrice}` 
 }
