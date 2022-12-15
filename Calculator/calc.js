d808a7c3eccc71679e4961454ca80fec49f37964
Melanie Werner <melanie.werner@fluid.contitech.de>
2022-12-15 11:40:53 +0100

    Calculator Store

---
+++ Calculator/calc.js
@@ -0,0 +1,16 @@
+let articleIdInput = document.getElementById("articleId")
+let numberOfArticlesInput = document.getElementById("numberOfArticles")
+let totalPriceOutput = document.getElementById("totalPrice")
+
+function calculatePrice(){
+    let articleId = articleIdInput.value
+    let numberOfArticles = parseInt(numberOfArticlesInput.value)
+    let totalPrice = 42
+    let result = createTotalPriceMessage(articleId, numberOfArticles, totalPrice)
+    totalPriceOutput.value = result
+
+}
+
+function createTotalPriceMessage(articleId, numberOfArticles, totalPrice){
+    return `total price: ${numberOfArticles} calc ${articleId} cost: ${totalPrice}` 
+}
