22d95e44a353a4cb62774442fc8329806a5cabca
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-12-15 09:19:56 +0100

    play with objects

--- software/demo/demo.js
+++ software/demo/demo.js
@@ -1,19 +1,22 @@
-let person = {
-    lastname: "Sawitzki", 
-    firstname: "Rainer"
+let discounts = {
+    no: 0,
+    low: 0.05,
+    high: 0.1
 }
 
-console.log(person.lastname)
-let first = person.firstname
-console.log(first)
-person.height = 183
-console.log(person.height)
+console.log(discounts.high) //Ausgabe des großen Rabatts
 
-function sayHello(name){
-    return "Hello " + name
+let discounts2 = discounts 
+
+discounts2.high = 0.2
+
+console.log(discounts2.high) //Ausgabe des geänderten Rabatts
+console.log(discounts.high) //Was wird hier ausgegeben? 2 Möglichkeiten: 0.1 oder 0.2
+
+function playWithDiscounts(d){
+    d.low = 0.01
+    console.log(d.low)
 }
 
-console.log(sayHello("Hugo"))
-console.log(sayHello("Emil"))
-let helloMessage = sayHello(person.lastname)
-console.log(helloMessage)
+playWithDiscounts(discount) //Was wird hier ausgegeben?
+console.log(discounts.low) //Was wird hier ausgegeben?
