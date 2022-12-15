7f11e358539196f12f6fdd24b9b606dd30036d55
Detlef Roeder <detlef.roeder@continental.com>
2022-12-15 10:33:06 +0100

    stg1

--- software/telephone-order/telephone.js
+++ software/telephone-order/telephone.js
@@ -6,7 +6,7 @@ function userInput(){
     let itemCount = parseInt(input)
     if (itemCount > 0) {
         let totalPrice = calculatePrice(itemCount)
-        resultElement.value = "Total price is " + totalPrice
+        resultElement.value = "Total price: " itemcount "articles with id " + "cost "+ totalPrice
     } else {
         resultElement.value = "invalid input"
     }
@@ -18,19 +18,21 @@ function clearInput(){
 }
 
 function calculatePrice(numberOfItems){
-    let discounts = {
-        no: 0,
-        low: 0.05,
-        high: 0.1
-    }
-    let discount = discounts.no
-    if (numberOfItems >= 10 && numberOfItems < 20) {
-        discount = discounts.low
-    } else if (numberOfItems >= 20) {
-        discount = discounts.high
-    }
-    let itemPrice = 42
-    let totalPrice = itemPrice * numberOfItems * (1 - discount)
+    // let discounts = {
+    //     no: 0,
+    //     low: 0.05,
+    //     high: 0.1
+    // }
+    // let discount = discounts.no
+    // if (numberOfItems >= 10 && numberOfItems < 20) {
+    //     discount = discounts.low
+    // } else if (numberOfItems >= 20) {
+    //     discount = discounts.high
+    // }
+    // let itemPrice = 42
+    let totalPrice = 42,01
+
+    // let totalPrice = itemPrice * numberOfItems * (1 - discount)
     return totalPrice
 
 }
