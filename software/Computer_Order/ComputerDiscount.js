567a0bb448945e57c643d2fb0f3e07b5fbd8260a
Tobias Haas <tobias.haas@continental.com>
2022-12-16 11:24:54 +0100

    add order.js + update

--- software/Computer_Order/ComputerDiscount.js
+++ software/Computer_Order/ComputerDiscount.js
@@ -2,22 +2,21 @@
 // discount calculation for computer order
 //*****************************************
 
-// discount and threshold definition
-discount = {
-    no: 0,
-    low: 0.1,
-    high: 0.2,
-    lowLimit: 1000,
-    highLimit: 5000
-}
 
-// calculate discounted price funktion
-function applyDiscount(totalPrice){
-    let price = totalPrice;
-    if (totalPrice >= discount.lowLimit && totalPrice < discount.highLimit){
-        price = totalPrice * (1 - discount.low)
-    }else if (totalPrice >= discount.highLimit){
-        price = totalPrice * (1 - discount.high)
+let discount = {
+    // discount and threshold definition
+	no: 0,	low: 0.1,high: 0.2,
+	lowLimit:1000, highLimit: 5000,
+
+    // calculate discounted price funktion
+    apply: function (totalPrice){
+        let price = totalPrice;
+        if (totalPrice >= this.lowLimit && totalPrice < this.highLimit){
+            price = totalPrice * (1 - this.low)
+        }else if (totalPrice >= this.highLimit){
+            price = totalPrice * (1 - this.high)
+        }
+        return price;
     }
-	return price;
+
 }
