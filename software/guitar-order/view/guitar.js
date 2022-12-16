c89412b9efd2cd22caea0406e73ff32d970a064d
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-12-16 15:36:18 +0100

    finish guitar

--- software/guitar-order/view/guitar.js
+++ software/guitar-order/view/guitar.js
@@ -1,11 +1,23 @@
 const guitar = {
-    articleIdInput: document.getElementById("articleId"),
+    
+    articleIdSelect: document.getElementById("articleId"),
     numberOfArticlesInput: document.getElementById("numberOfArticles"),
-    totalPriceOutput: document.getElementById("totalPrice"),
+    orders: document.getElementById("orders"),
     doOrder: function(){
-        let articleId = this.articleIdInput.value
+        let articleId = this.articleIdSelect[this.articleIdSelect.selectedIndex].value
         let numberOfArticles = parseInt(this.numberOfArticlesInput.value)
         let totalPriceMessage = order.calculatePrice(articleId, numberOfArticles)
-        this.totalPriceOutput.value = totalPriceMessage
+        this.appendOrderToList(totalPriceMessage)
+    },
+
+    appendOrderToList: function(orderMessage){
+        let newLi = document.createElement("li")
+        newLi.innerHTML = orderMessage
+        this.orders.appendChild(newLi)
     }
+}
+
+for (const [key, value] of Object.entries(articles)){
+    console.log(key)
+    guitar.articleIdSelect.add(new Option(key))
 }
