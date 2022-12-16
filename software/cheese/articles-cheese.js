97840d4f87ff0e9d8e3f941035dda012084973c3
Harald <harald.zimmermann@continental-corporation.com>
2022-12-16 14:32:39 +0100

    mit Eingabe

---
+++ software/cheese/articles-cheese.js
@@ -0,0 +1,12 @@
+const articles = {
+"id-123":{description: "Camenbert", price: 1.99},
+"id-234":{description: "Emmentaler", price: 2.99},
+"id-345":{description: "Gauda", price: 3.99},
+}
+
+//let articleId = prompt()
+//let article = articles[articleId] // Objekt mit Beschreibung und Preis
+//if (article) {let articlePrice = article.price
+//console.log (articlePrice)}
+//else
+//console.log ("Artikel nicht gefunden")
