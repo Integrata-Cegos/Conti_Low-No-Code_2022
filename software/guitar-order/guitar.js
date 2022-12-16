c74fa3051b9482951c036d9f5d53ff6652a6b8f3
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-12-16 10:09:40 +0100

    implement guitar discount

--- software/guitar-order/guitar.js
+++ software/guitar-order/guitar.js
@@ -1,13 +1,14 @@
 let articleIdInput = document.getElementById("articleId")
 let numberOfArticlesInput = document.getElementById("numberOfArticles")
 let totalPriceOutput = document.getElementById("totalPrice")
-
+const articlePrice = 100
 function calculatePrice(){
     let articleId = articleIdInput.value
     let numberOfArticles = parseInt(numberOfArticlesInput.value)
-    let totalPrice = 42
-    let result = createTotalPriceMessage(articleId, numberOfArticles, totalPrice)
-    totalPriceOutput.value = result
+    let totalPrice = numberOfArticles * articlePrice
+    totalPrice = applyDiscount(totalPrice)
+    let totalPriceMessage = createTotalPriceMessage(articleId, numberOfArticles, totalPrice)
+    totalPriceOutput.value = totalPriceMessage
 
 }
 
