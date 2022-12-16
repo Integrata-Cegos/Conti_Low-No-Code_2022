bd0c43ac57254f6636f46e4e0ed4d644a4cf7fcf
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-12-16 14:23:40 +0100

    implement articles

--- software/guitar-order/app/articles.js
+++ software/guitar-order/app/articles.js
@@ -1,14 +1,7 @@
 const articles = {
-	"id-123": {description: "Lesley xyz", price: 29.99},
-	"id-124": {description: "Höhner", price: 666.66},
-
+	"id-123": {description: "Fender", price: 999.97},
+	"id-124": {description: "Höfner", price: 6.66},
+	"id-125": {description: "Yamaha", price: 555.66},
+	"id-126": {description: "Lesley", price: 6.66},
 }
 
-
-let articleId = prompt()
-let article = articles[articleId]
-if (article){
-    let articlePrice = article.price
-    console.log(articlePrice)
-}
-console.log("Artikel nicht gefunden")
