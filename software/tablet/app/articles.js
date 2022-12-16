9b9bdbac561f3b73c25db4727f448cdc4f29f6c2
Linh Nguyen <thuy.linh.nguyen@continental.com>
2022-12-16 14:31:07 +0100

    change

---
+++ software/tablet/app/articles.js
@@ -0,0 +1,6 @@
+const articles = {
+	"id-123": {description: "Apple", price: 499.99},
+	"id-124": {description: "Samsung", price: 599.99},
+	"id-125": {description: "HTC", price: 239.99},
+	"id-126": {description: "HP", price: 450.37},
+}
