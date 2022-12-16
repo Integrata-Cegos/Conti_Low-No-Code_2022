31cfd123b9dbc27b4345effdaf448e8b72e7c60f
Detlef Roeder <detlef.roeder@continental.com>
2022-12-16 10:18:41 +0100

    del paste

--- software/telephone-order/discount.js
+++ software/telephone-order/discount.js
@@ -1,20 +1,3 @@
-let articleIdInput = document.getElementById("articleId")
-let numberOfArticlesInput = document.getElementById("numberOfArticles")
-let totalPriceOutput = document.getElementById("totalPrice")
-
-function calculatePrice(){
-    let articleId = articleIdInput.value
-    let numberOfArticles = parseInt(numberOfArticlesInput.value)
-    let totalPrice = 42
-    let result = createTotalPriceMessage(articleId, numberOfArticles, totalPrice)
-    totalPriceOutput.value = result
-
-}
-
-function createTotalPriceMessage(articleId, numberOfArticles, totalPrice){
-    return "total price: " +numberOfArticles + " articles with id " + articleId +" cost: " +totalPrice 
-   
-}
 let discount ={
     no: 0, low: 0.05, high: 0.1
     lowlimit:400, highlimit:1000
