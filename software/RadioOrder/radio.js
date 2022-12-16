18c5d30df3831434d4c230a3c145b9f68cdc125b
Frederic Loeher <a@b.de>
2022-12-16 14:50:57 +0100

    div implementations

--- software/RadioOrder/radio.js
+++ software/RadioOrder/radio.js
@@ -14,4 +14,14 @@ function calculatePrice(){
 
 function createTotalPriceMessage(articleId, numberOfArticles, totalPrice){
     return `total price: ${numberOfArticles} articles with id ${articleId} cost: ${totalPrice}` 
+}const radio = {
+    articleIdInput: document.getElementById("articleId"),
+    numberOfArticlesInput: document.getElementById("numberOfArticles"),
+    totalPriceOutput: document.getElementById("totalPrice"),
+    doOrder: function(){
+        let articleId = this.articleIdInput.value
+        let numberOfArticles = parseInt(this.numberOfArticlesInput.value)
+        let totalPriceMessage = order.calculatePrice(articleId, numberOfArticles)
+        this.totalPriceOutput.value = totalPriceMessage
+    }
 }
--- software/RadioOrder/radioDiscount.js
+++ software/RadioOrder/radioDiscount.js
@@ -1,15 +1,16 @@
 let discount = {
 	no: 0,	low: 0.1,high: 0.2,
-	lowLimit:1000, highLimit: 5000
+	lowLimit:1000, highLimit: 5000,
+    apply: function (totalPrice){
+        let price = totalPrice;
+        if (totalPrice >= this.lowLimit && totalPrice < this.highLimit){
+            price = totalPrice * (1 - this.low)
+        }else if (totalPrice >= this.highLimit){
+            price = totalPrice * (1 - this.high)
+        }
+        return price;
+    }
+
 }
 
-function applyDiscount(totalPrice){
-    let price = totalPrice;
-    if (totalPrice >= discount.lowLimit && totalPrice < discount.highLimit){
-        price = totalPrice * (1 - discount.low)
-    }else if (totalPrice >= discount.highLimit){
-        price = totalPrice * (1 - discount.high)
-    }
-	return price;
-}
 
