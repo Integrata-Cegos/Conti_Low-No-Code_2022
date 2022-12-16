f6fa4083f7ef11c4b431da799e4b9019eb8865c4
Tanja Brost <tanja.brost@continental-corporation.com>
2022-12-16 12:06:49 +0100

    modify program structure

--- software/CD-Order/CDdiscount.js
+++ software/CD-Order/CDdiscount.js
@@ -1,14 +1,13 @@
-let discounts = {
+let discount = {
     no: 0, low: 0.1, high: 0.2,
-    lowLimit: 10, highLimit: 50
-}
-
-function applyDiscount(totalPrice) {
-let price = totalPrice
-if (totalPrice >= discounts.lowLimit && totalPrice < discounts.highLimit) {
-    price = totalPrice* (1-discounts.low)
-} else if (totalPrice >= discounts.highLimit) {
-    price = totalPrice* (1-discounts.high)
-}
-    return price
+    lowLimit: 10, highLimit: 50,
+    apply: function (totalPrice){
+        let price = totalPrice;
+        if (totalPrice >= this.lowLimit && totalPrice < this.highLimit){
+            price = totalPrice * (1 - this.low)
+        }else if (totalPrice >= this.highLimit){
+            price = totalPrice * (1 - this.high)
+        }
+        return price;
+    }
 }
--- software/CD-Order/cdOrder.js
+++ software/CD-Order/cdOrder.js
@@ -2,17 +2,11 @@ let resultElement = document.getElementById("totalPrice")
 let inputQuantity = document.getElementById("quantity")
 let inputItem = document.getElementById("item") 
 
-function calculatePrice() {
-    let item = inputItem.value
-    let input = inputQuantity.value
-    let itemCount = parseInt(input)
-    let totalPrice = itemCount* 1
-    totalPrice= applyDiscount(totalPrice)
-    let result  = createTotalPriceMessage(item, itemCount, totalPrice)
-    resultElement.value = result
+function doOrder(){
+    let articleId = inputItem.value
+    let numberOfArticles = parseInt(inputQuantity.value)
+    let totalPriceMessage = order.calculatePrice(articleId, numberOfArticles)
+    resultElement.value = totalPriceMessage
+
 }
-    
-function createTotalPriceMessage(inputItem, itemCount, totalPrice){
-    return `total price: ${itemCount} articles with id ${inputItem} cost: ${totalPrice}` 
-}
-    
+  
