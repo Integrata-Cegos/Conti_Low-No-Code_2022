e862f487e7c24cfabd3956cb2da0a371a5b816d7
Detlef Roeder <detlef.roeder@continental.com>
2022-12-16 12:25:43 +0100

    re

--- software/telephone-order/discount.js
+++ software/telephone-order/discount.js
@@ -1,32 +1,18 @@
-// let discount ={
-//     no: 0, low: 0.05, high: 0.1,
-//     lowlimit:400, highlimit:1000
-// }
 
-// function applydiscount(totalprice){
-//     let price=totalprice
-//     if (totalprice>=discount.highlimit){
-//         price=totalprice*(1-discount.high)
-//     }else if (totalprice>=discount.lowlimit){
-//         price=totalprice*(1-discount.low)
-//     }  
-// return price;
-// }
 
-let discount = {
-	no: 0,	low: 0.05,high: 0.1,
-	lowLimit:400, highLimit: 1000,
-    apply: function (totalPrice){
-        let price = totalPrice;
-        if (totalPrice >= this.lowLimit && totalPrice < this.highLimit){
-            price = totalPrice * (1 - this.low)
-        }else if (totalPrice >= this.highLimit){
-            price = totalPrice * (1 - this.high)
-        }
-        return price;
-    }
-
+let discount ={
+    no: 0, low: 0.05, high: 0.1,
+    lowlimit:400, highlimit:1000
 }
 
+function applydiscount(totalprice){
+    let price=totalprice
+    if (totalprice>=discount.highlimit){
+        price=totalprice*(1-discount.high)
+    }else if (totalprice>=discount.lowlimit){
+        price=totalprice*(1-discount.low)
+    }  
+return price;
+}
 
 
