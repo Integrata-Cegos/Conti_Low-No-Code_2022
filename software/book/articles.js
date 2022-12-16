34f96fef7070d05f28aba9f43b2ed209a59ed0c9
Hayrettin Baser <hayrettin.baser@continental.com>
2022-12-16 14:47:21 +0100

    add articles

---
+++ software/book/articles.js
@@ -0,0 +1,6 @@
+const articles = {
+    "id-100" : { description: "Herr der Ringe", price: 59.99},
+    "id-101" : { description: "Der Hobbit", price: 29.99},
+    "id-102" : {description: "Das Silmarillion", price: 99.99}
+}
+
