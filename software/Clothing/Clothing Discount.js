f71ca0e2e7717fb85b55b9c438e3e9be54a912a5
Rustler Guenther <guenther.rustler@continental.com>
2022-12-16 10:55:54 +0100

    Clothing with Sample

--- software/Clothing/Clothing Discount.js
+++ software/Clothing/Clothing Discount.js
@@ -1,17 +1,14 @@
-function calculatePrice(numberOfItems){
-    let discounts = {
-        no: 0,
-        low: 0.1,
-        high: 0.2
+let discount = {
+	no: 0,	low: 0.1,high: 0.2,
+	lowLimit:1000, highLimit: 5000,
+    apply: function (totalPrice){
+        let price = totalPrice;
+        if (totalPrice >= this.lowLimit && totalPrice < this.highLimit){
+            price = totalPrice * (1 - this.low)
+        }else if (totalPrice >= this.highLimit){
+            price = totalPrice * (1 - this.high)
+        }
+        return price;
     }
-    let discount = discounts.no
-    if (numberOfItems >= 10 && numberOfItems < 20) {
-        discount = discounts.low
-    } else if (numberOfItems >= 20) {
-        discount = discounts.high
-    }
-    let itemPrice = 100
-    let totalPrice = itemPrice * numberOfItems * (1 - discount)
-    return totalPrice
 
 }
