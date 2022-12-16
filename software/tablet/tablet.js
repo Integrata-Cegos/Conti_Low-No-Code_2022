3458d8a3df682c58acf8bba51934fc3b0de945ef
Linh Nguyen <thuy.linh.nguyen@continental.com>
2022-12-16 11:41:08 +0100

    change content

--- software/tablet/tablet.js
+++ software/tablet/tablet.js
@@ -1,16 +1,10 @@
 let articleIdInput = document.getElementById("articleId")
 let numberOfArticlesInput = document.getElementById("numberOfArticles")
 let totalPriceOutput = document.getElementById("totalPrice")
-
-function calculatePrice(){
+function doOrder(){
     let articleId = articleIdInput.value
     let numberOfArticles = parseInt(numberOfArticlesInput.value)
-    let totalPrice = 100
-    let result = createTotalPriceMessage(articleId, numberOfArticles, totalPrice)
-    totalPriceOutput.value = result
+    let totalPriceMessage = order.calculatePrice(articleId, numberOfArticles)
+    totalPriceOutput.value = totalPriceMessage
 
-}
-
-function createTotalPriceMessage(articleId, numberOfArticles, totalPrice){
-    return `total price: ${numberOfArticles} articles with id ${articleId} cost: ${totalPrice}` 
 }
