a87daa96c2c8a93c4b080b63b22b34969ec88e52
Hayrettin Baser <hayrettin.baser@continental.com>
2022-12-16 12:10:27 +0100

    korrektur

--- software/book/order.js
+++ software/book/order.js
@@ -10,5 +10,4 @@ const order = {
     createTotalPriceMessage: function (articleId, numberOfArticles, totalPrice) {
         return `total price: ${numberOfArticles} articles with id ${articleId} cost: ${totalPrice}`
     }
-
 }
