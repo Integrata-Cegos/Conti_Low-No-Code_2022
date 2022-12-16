a0f8a577449594ed15c829cdfc95b113fadf21de
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-12-16 12:09:05 +0100

    cleanup

---
+++ software/guitar-order/view/guitar.js
@@ -0,0 +1,11 @@
+const guitar = {
+    articleIdInput: document.getElementById("articleId"),
+    numberOfArticlesInput: document.getElementById("numberOfArticles"),
+    totalPriceOutput: document.getElementById("totalPrice"),
+    doOrder: function(){
+        let articleId = this.articleIdInput.value
+        let numberOfArticles = parseInt(this.numberOfArticlesInput.value)
+        let totalPriceMessage = order.calculatePrice(articleId, numberOfArticles)
+        this.totalPriceOutput.value = totalPriceMessage
+    }
+}
