94a0484ac0d81c6f0ca5804d42e75c9cb09f97c6
Hayrettin Baser <hayrettin.baser@continental.com>
2022-12-16 11:36:52 +0100

    add order.js

--- software/book/discount.js
+++ software/book/discount.js
@@ -1,17 +1,14 @@
 let discount = {
 	no: 0,	low: 0.1,high: 0.2,
-	lowLimit:1000, highLimit: 5000
-}
-
-function applyDiscount(totalPrice){
-    let price = totalPrice;
-    if (totalPrice >= discount.lowLimit && totalPrice < discount.highLimit){
-        price = totalPrice * (1 - discount.low)
-    }else if (totalPrice >= discount.highLimit){
-        price = totalPrice * (1 - discount.high)
+	lowLimit:1000, highLimit: 5000,
+    apply: function (totalPrice){
+        let price = totalPrice;
+        if (totalPrice >= this.lowLimit && totalPrice < this.highLimit){
+            price = totalPrice * (1 - this.low)
+        }else if (totalPrice >= this.highLimit){
+            price = totalPrice * (1 - this.high)
+        }
+        return price;
     }
-	return price;
+
 }
-
-
-
