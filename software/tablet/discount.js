bbc36caba84c28b8a32be76b4b541ca89f25aebf
Linh Nguyen <thuy.linh.nguyen@continental.com>
2022-12-16 11:53:12 +0100

    change

--- software/tablet/discount.js
+++ software/tablet/discount.js
@@ -1,6 +1,6 @@
 let discount = {
 	no: 0,	low: 0.1,high: 0.2,
-	lowLimit:1000, highLimit: 5000,
+	lowLimit:1000, highLimit: 2000,
     apply: function (totalPrice){
         let price = totalPrice;
         if (totalPrice >= this.lowLimit && totalPrice < this.highLimit){
