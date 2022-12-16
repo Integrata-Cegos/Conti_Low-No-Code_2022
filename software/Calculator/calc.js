9910ccb2d822323368a41d6e1992fb62377eebba
Melanie Werner <melanie.werner@fluid.contitech.de>
2022-12-16 11:31:23 +0100

    Latest Updates

--- software/Calculator/calc.js
+++ software/Calculator/calc.js
@@ -1,17 +1,10 @@
 let articleIdInput = document.getElementById("articleId")
 let numberOfArticlesInput = document.getElementById("numberOfArticles")
 let totalPriceOutput = document.getElementById("totalPrice")
-const articlePrice = 100
-function calculatePrice(){
+function doOrder(){
     let articleId = articleIdInput.value
     let numberOfArticles = parseInt(numberOfArticlesInput.value)
-    let totalPrice = numberOfArticles * articlePrice
-    totalPrice = discount.apply(totalPrice)
-    let totalPriceMessage = createTotalPriceMessage(articleId, numberOfArticles, totalPrice)
+    let totalPriceMessage = order.calculatePrice(articleId, numberOfArticles)
     totalPriceOutput.value = totalPriceMessage
 
-}
-
-function createTotalPriceMessage(articleId, numberOfArticles, totalPrice){
-    return `total price: ${numberOfArticles} article ${articleId} cost: ${totalPrice}` 
 }
