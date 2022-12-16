5aac422a8da97041b99a5b2d3cfe884835bccf5c
Birgit Leunig <birgit.leunig@continental.com>
2022-12-16 14:41:29 +0100

    implement artciles

--- software/TV/order.js
+++ software/TV/order.js
@@ -1,14 +1,19 @@
 const order = {
-    articlePrice: 100,
     calculatePriceTV: function (articleId, quantity) {
-        let result = quantity * this.articlePrice
-        result = discount.apply(result)
-        let totalPriceMessage = this.createTotalPriceMessage(articleId, quantity, result)
-        return totalPriceMessage
+        let article = articles [articleId]
+        if (article) {
+            let result = quantity * article.price
+            result = discount.apply(result)
+            let totalPriceMessage = this.createTotalPriceMessage(articleId, quantity, result)
+            return totalPriceMessage
+        }
+        else {
+            return `article with id ${articleId} not found`
+        }
 
     },
-    createTotalPriceMessage: function (articleId, quantity, result) {
-        return `total price: ${quantity} articles with id ${articleId} cost: ${result}`
+    createTotalPriceMessage: function (article, quantity, result) {
+        return `total price: ${quantity} articles: ${article.description} cost: ${result} EURO`
     }
 
 }
