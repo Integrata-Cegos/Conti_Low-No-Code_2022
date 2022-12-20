8700ec1322f5883456879aa146c3f6422ec35a46
Detlef Roeder <detlef.roeder@continental.com>
2022-12-20 15:40:51 +0100

    Upper; lower; Anzeige mit PrioStufe

--- todo.js
+++ todo.js
@@ -1,19 +1,33 @@
 const list = {
 
     inputElement: document.getElementById("input"),
+    prioInput: document.getElementById("prioInput"),
     outputElement: document.getElementById("output"),
     
     DoAdd:  function(){
         let listItem = this.inputElement.value
+        let Prio = this.prioInput.value
         // let totalList = listItem
         // this.outputElement.value = totalList
-        this.appendItemToList(listItem)
+        this.createList(listItem,Prio)
+        // this.appendItemToList(listItem,prioInput)
+        console.log (listItem,Prio,'in Add')
     },
     
-    appendItemToList: function(listEntry){
-        let newLi = document.createElement("li")
-        newLi.innerHTML = listEntry
-        this.outputElement.appendChild(newLi)
+    createList: function(listEntry,Prio){
+        let List = document.createElement("li")
+        if (Prio == "low"){
+            List.innerHTML = listEntry.toLowerCase() +' Prio:   '+Prio}
+        if (Prio == "HIGH"){
+            List.innerHTML = listEntry.toUpperCase() +' Prio:   '+Prio}
+        if (Prio == "medium"){
+            List.innerHTML = listEntry +' Prio:   '+Prio}
+
+        let outputMsg=this.outputElement
+        console.log (outputMsg,' List in createList')
+        console.log (Prio,' Prio in createList')
+        console.log (this.outputElement,'intoList')
+        this.outputElement.appendChild(List)
     },
     
     DoClear: function() {
