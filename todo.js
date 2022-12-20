55ebbd9dbe0d93055582f92bf813a901a613b953
Detlef Roeder <detlef.roeder@continental.com>
2022-12-20 12:31:25 +0100

    clear; list; input

---
+++ todo.js
@@ -0,0 +1,26 @@
+const list = {
+
+    inputElement: document.getElementById("input"),
+    outputElement: document.getElementById("output"),
+    
+    DoAdd:  function(){
+        let listItem = this.inputElement.value
+        // let totalList = listItem
+        // this.outputElement.value = totalList
+        this.appendItemToList(listItem)
+    },
+    
+    appendItemToList: function(listEntry){
+        let newLi = document.createElement("li")
+        newLi.innerHTML = listEntry
+        this.outputElement.appendChild(newLi)
+    },
+    
+    DoClear: function() {
+            while (this.outputElement.hasChildNodes()) {
+                this.outputElement.removeChild(this.outputElement.firstChild)
+            }
+    
+    }
+    
+    }
