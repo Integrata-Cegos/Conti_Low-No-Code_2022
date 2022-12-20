d37f7bb839b61e153a22c71b477e28072674a95d
Harald <harald.zimmermann@continental-corporation.com>
2022-12-20 10:26:19 +0100

    der Anfang

---
+++ todo_hazi.js
@@ -0,0 +1,7 @@
+let inputElement = document.getElementById("newEntryInput")
+let outputElement = document.getElementById("newEntryOutput")
+
+function addInput(){
+    let input = inputElement.value
+    outputElement.value = input
+}
