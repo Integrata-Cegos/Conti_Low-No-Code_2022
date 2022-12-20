2dd9d272d4a6b3ecd8b7ba0e80ca95836e1b005e
Harald <harald.zimmermann@continental-corporation.com>
2022-12-20 15:24:08 +0100

    wird heute nix

--- todo_hazi.js
+++ todo_hazi.js
@@ -1,14 +1,26 @@
 let inputElement = document.getElementById("newEntryInput")
 let outputElement = document.getElementById("newEntryOutput")
 let auswahl = document.getElementById("auswahlFeld")
+//let outputListe = document.getElementById("outputListe")
 
 function addInput(){
     let input = inputElement.value
-    let wert = auswahl.value
-    if (wert == "medium"){outputElement.value = input}
-    else if (wert == "high"){outputElement.value = input.toUpperCase()}
-    else {outputElement.value = input.toLowerCase()}
+    if (auswahl.value == "medium"){outputElement.value = input}
+    else if (auswahl.value == "high"){outputElement.value = input.toUpperCase()}
+   else {outputElement.value = input.toLowerCase()}
 }
+
+//function addInput(){
+    //let input = inputElement.value
+    //outputElement.value = input
+//}
+
+//   function (ausgabe){
+    //let newLi = document.createElement("Li")
+    //newLi.innerHTML = ausgabe
+    //this.outputListe.appenChild(newLi)
+
+//}
 
 function deleteInput(){
     outputElement.value = ""
