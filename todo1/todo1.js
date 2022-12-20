8da1d0314b30e99d3db0f79a47977398ef9971a9
Maximilian Patzer <maximilian.patzer@continental.com>
2022-12-20 15:11:39 +0100

    Prio Buchstaben

---
+++ todo1/todo1.js
@@ -0,0 +1,36 @@
+const updateList = {
+
+    prioInput: document.getElementById(`prioInput`),
+    entryInput: document.getElementById(`entryInput`),
+    outputMessage: document.getElementById(`outputMessage`),
+
+
+    addButton: function(){
+
+        let entryList = this.entryInput.value
+        let prioInput = this.prioInput.value
+
+        this.createList(entryList, prioInput)
+    },
+
+    createList: function(entryList, prioInput)  {
+    
+        let List = document.createElement("list")
+
+        if (prioInput == "low"){
+            List.innerHTML = entryList.toLowerCase()}
+        if (prioInput == "high"){
+            List.innerHTML = entryList.toUpperCase()}
+        if (prioInput == "medium"){
+            List.innerHTML = entryList}
+
+            this.outputMessage.appendChild(List)
+    },
+
+
+    clearButton: function () {
+        outputMessage.value = ""
+    },
+
+    
+}
