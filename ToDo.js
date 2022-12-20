939f7d5f0f8de8ca972bb5f2c57f2ebeb09d3411
Tanja Brost <tanja.brost@continental-corporation.com>
2022-12-20 10:29:38 +0100

    Add list with one entry

--- ToDo.js
+++ ToDo.js
@@ -5,9 +5,15 @@ outputElement: document.getElementById("output"),
 
 DoAdd:  function(){
     let listItem = this.inputElement.value
-    let totalList = listItem
-    this.outputElement.value = totalList
+    // let totalList = listItem
+    // this.outputElement.value = totalList
+    this.appendItemToList(listItem)
+},
 
+appendItemToList: function(listEntry){
+    let newLi = document.createElement("li")
+    newLi.innerHTML = listEntry
+    this.outputElement.appendChild(newLi)
 }
 
 }
