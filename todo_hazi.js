176ee797c3a2cfec6ac0b6645389edaff1a6a372
Harald <harald.zimmermann@continental-corporation.com>
2022-12-20 13:43:55 +0100

    mit Prio

--- todo_hazi.js
+++ todo_hazi.js
@@ -1,10 +1,14 @@
 let inputElement = document.getElementById("newEntryInput")
 let outputElement = document.getElementById("newEntryOutput")
+let auswahl = document.getElementById("auswahlFeld")
 
 function addInput(){
     let input = inputElement.value
-    outputElement.value = input
-}
+    let wert = auswahl.value
+    if (wert == "medium"){outputElement.value = input}
+    else if (wert == "high"){outputElement.value = input.toUpperCase()}
+    else {outputElement.value = input.toLowerCase()}
+
 
 function deleteInput(){
     outputElement.value = ""
