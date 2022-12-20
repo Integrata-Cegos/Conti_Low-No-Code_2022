9504f8171c4e9505075c918ebc1a067570af0c4b
Markus Engelke <markus.engelke@conti.com>
2022-12-20 15:02:47 +0100

    Liste

--- Todo.js
+++ Todo.js
@@ -1,6 +1,37 @@
-function addToList(){
-    let todo = document.getElementById("todo")
-    let todoList = document.createElement("todoList")
-    todoList.innerHTML = todo
-    return todoList
+const act = {
+
+    prio: document.getElementById("prio"),
+    todo: document.getElementById("todo"),
+    todoListe: document.getElementById("todoList"),
+
+
+    addToList: function(){
+
+        let newEntry = this.todo.value
+        let prio = this.prio.value
+
+        this.createList(newEntry, prio)
+    },
+
+    createList: function(newEntry, prio)  {
+        
+        let List = document.createElement("li")
+
+
+        if (prio == "low"){
+            List.innerHTML = newEntry.toLowerCase()}
+        if (prio == "high"){
+            List.innerHTML = newEntry.toUpperCase()}
+        else {
+            List.innerHTML = newEntry}
+
+        this.todoList.appendChild(List)
+    },
+
+
+    deleteList: function () {
+        todoList.value = ""
+    },
+
+    
 }
